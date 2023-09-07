ff0cf381ca7ecab1fa32fa23bad1267620d69fca
riomonet <arizablozki@gmail.com>
2023-09-07 00:48:17 -0400

    add feature tests

---
+++ cuc_test/features/steps.js
@@ -0,0 +1,20 @@
+const {Given, When, Then} = require('@cucumber/cucumber')
+
+  
+Given('Lucy is located {int} metres from Sean', function (distance) {
+    console.log(distance)
+         // Given('Lucy is located {float} metres from Sean', function (float) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+         When('Sean shouts {string}', function (string) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+         Then('Lucy hears Sean\'s message', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
