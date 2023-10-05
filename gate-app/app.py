796192dfc62c7eb51eb625b9d77cbf3eb66eb302
riomonet <arizablozki@gmail.com>
2023-10-05 16:12:47 -0400

    add login response and eventlisteneres

--- gate-app/app.py
+++ gate-app/app.py
@@ -1,20 +1,21 @@
 
-from flask import Flask, request, render_template
-
-
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 
-# view function
-# @ indicates a python decorator 
-@app.route('/')
-def login():
-    return render_template('login.html')
 
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == 'ari' and request.form['pw'] == 'ari':
+                return redirect('/access')
+        else:
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
+    
 @app.route('/access')
 def access():
     return render_template('access.html')
-
-# to access query strings we need to import request.
-# query strings stored in request object in request.args(dictionary like object)
---
+++ gate-app/static/app.js
@@ -0,0 +1,10 @@
+
+v = document.querySelector("#vehicle")
+p = document.querySelector("#pedestrian")
+
+
+
+v.addEventListener('click', () => console.log("vehicle clicked") )
+p.addEventListener('click', () => console.log("pedestrian clicked") )
+
+// add function call to activate gate
